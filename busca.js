1204c712ec6624443f9665c41c44a8594556f76d
José Ruan <143905670+joseeruan@users.noreply.github.com>
2023-09-10 02:05:25 -0300

    valor corrigido

--- busca.js
+++ busca.js
@@ -1,3 +1,7 @@
+/*Temos o array [15, 8, 10, 25, 12, 30, 5, 20, 18, 7] e precisamos criar um código em JavaScript para encontrar o valor 20.
+Para isso, temos que escolher entre realizar uma busca linear ou binária de acordo com a que for mais adequada para essa situação.
+Codifique a solução mais eficiente para buscar o número 20 no array. 
+*/
 function buscarElemento(lista, elementoBuscado) {
     for (let i = 0; i < lista.length; i++) {
         if (lista[i] === elementoBuscado) {
@@ -9,4 +13,4 @@ function buscarElemento(lista, elementoBuscado) {
 
 let numeros = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7]
 
-buscarElemento(numeros, 15)
+buscarElemento(numeros, 20)
