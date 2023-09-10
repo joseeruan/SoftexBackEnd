633fdfe86320fe26daf8ef97da5b6328296e978e
José Ruan <jrpaivadev@gmail.com>
2023-09-10 02:00:55 -0300

    busca.js 1.0

---
+++ busca.js
@@ -0,0 +1,12 @@
+function buscarElemento(lista, elementoBuscado) {
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] === elementoBuscado) {
+            return console.log(`O elemento ${elementoBuscado} foi encontrado na posição ${i}`);
+        }
+    }
+    return console.log(`O elemento ${elementoBuscado} não foi encontrado na lista`);
+}
+
+let numeros = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7]
+
+buscarElemento(numeros, 15)
